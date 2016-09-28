b4ffbc05355061c8992fa0e271236db0ec3a227e
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-09-28 12:32:51 -0300

    Documented and wrote some cleaning functions

--- data_clean_sample_util.R
+++ data_clean_sample_util.R
@@ -5,45 +5,80 @@
 library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
 library(lazyeval, quietly = TRUE, warn.conflicts = FALSE)
 
-clean.smooth.subsequent <- function(table, column, n) {
-    nrows <- length(table[[column]])
-    table[['novo']] = 1:nrows
-    #for (i in 1:nrows) {
-    #    table[i, column] <- mean(table[i:min(c(i+n-1, nrows)), column]);
-    #}
-    table[[column]] <- mean(table[[ table[[column]]  ]])
-    table;
+# Apply a cleaning function to a data frame
+clean.apply <- function(table, f, column, ..., new.column = column ) {
+    table[[new.column]] <- f(table[[column]], ...)
+    table
 }
 
-clean.smooth.precedent <- function(table, column, n) {
-    nrows <- length(table[column][[1]]);
-    table[['novo']] = 1:nrows
-    for (i in nrows:1) {
-        table[i, 'novo'] <- mean(table[max(i-n+1, 1):i, column])
-    }
-    table;
+clean.smooth.subsequent <- function(v, n) {
+    # Smooth each point with its subsequents
+    # It is done from the begin to the end, so
+    # the smooth doesn't spread over the role vector
+    #
+    # Args:
+    #   v: the input vector, it must be 'summable'
+    #   n: the number of subsequent elements to use in the smooth
+    #
+    # Returns:
+    #   A new vector with the subsequent smooth applied
+    
+    sapply(seq_len(length(v)), function(i) {
+        mean(v[i:min(length(v), i + n)])
+    })
 }
 
-clean.transform.timestamp <- function(table, timestamp.column, date.columns = c("hour")) {
-
+clean.smooth.precedent <- function(v, n) {
+    # Smooth each point with its precedents
+    # It is done from the end to the begin, so
+    # the smooth doesn't spread over the role vector    
+    # Args:
+    #   v: the input vector, it must be 'summable'
+    #   n: the number of precedent elements to use in the smooth
+    #
+    # Returns:
+    #   A new vector with the precedent smooth applied
+    
+    sapply(seq_len(length(v)), function(i) {
+        mean(v[max(1, i - n):i])
+    })
 }
 
-clean.transform.edge <- function(table, column, new.column) {
-    # Extracts the edges from the column, which becomes a FACTOR
-    # Factors are named after the respective new values
-    # When there is no change in value, the factor NO_CHANGE is used.
+clean.transform.timestamp <- function(v, date.info = "hour") {
+    # Transforms a timestamp vector in a vector with 
+    # a date info (like hour, day, ...)
+    # 
+    # Args:
+    #   v: the input vector, it must contain timestamps
+    #   date.info: string with the date info to extract. Can contain
+    #              "hour" and "day". Its default is "hour".
+    #
+    # Returns:
+    #   The vector with the desired info
+    
+    as.POSIXlt(v, origin = "1970-09-01")[[date.info]]
+}
 
-    # Creates new column with next state of column
-    table[[new.column]] <- c(tail(table[[column]], -1), NA)
+clean.transform.edge <- function(v) {
+    # Extracts the edges (when there is change in the vector)
+    #
+    # Args:
+    #   v: the vector to extract the edges, can be of any type,
+    #      but must be a comparable type
+    #
+    # Retunrs:
+    #   A new factor with the same length as v with NO_ACTION
+    #   when there isn't action and <VALUE> when the next point
+    #   assumes the value <VALUE>
+    
+    
+    # Shift the input, to get the next action
+    shiftedColumn <- c(tail(v, -1), NA)
 
     # Compare current value with next and create factors
-    table[[new.column]] <- as.factor(
-        ifelse(is.na(table[[new.column]]) | is.na(table[[column]]) |
-        table[[new.column]] == table[[column]],
-        'NO_CHANGE', table[[new.column]]))
-    
-    # Returns table
-    table
+    as.factor(
+        ifelse(is.na(shiftedColumn) | is.na(v) |
+            shiftedColumn == v, 'NO_ACTION', shiftedColumn))
 }
 
 
