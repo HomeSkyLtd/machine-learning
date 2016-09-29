7e1fcd346b7cbb1d223a69f7e2164f27557eba9d
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-09-29 13:01:02 -0300

    Some testing with tree-based libraries

---
+++ nodes_util.R
@@ -0,0 +1,8 @@
+# Utility functions for nodes data, command and state
+
+nodes.metadata.transform <- function(nodes.metadata) {
+}
+
+node.state.expand <- function(input, nodes.metadata) {
+    
+}
--- test/light_clean.R
+++ test/light_clean.R
@@ -6,16 +6,48 @@
 
 #inputFile <- args[1]
 #tbl <- read.table(inputFile)
-
-
-source("../data_clean_sample_util.R")
     
-tbl <- data.frame(light = c(0,0,0,50,50,49,0,0,0), presence = c(0,0,1,1,1,1,1,0,0), 
-    light_on = c(0,0,0,1,1,2,0,0,0))
+library(tree)
+library(rpart)
 
-tbl <- clean.transform.edge(tbl, "light_on", "action")
 
-tbl <- clean.smooth.subsequent(tbl, 'light', 3)
+table <- read.table('test/data/raw/ricardo_lamp.txt', header = TRUE)
 
-tbl
+# Remove timestamp
+
+table <- select(table, -timestamp)
+# Smooth presence
+table <- clean.apply(table, clean.smooth.subsequent, 'presence', n = 20)
+# Extract action
+table <- clean.apply(table, clean.transform.edge, 'light_on',new.column = 'action')
+
+print(head(table))
+
+train_and_print <- function(tbl) {
+    
+    #tbl <- mutate(tbl, light_on = as.factor(light_on))
+    tbl <- mutate(tbl, dummy = rnorm(nrow(tbl)), 
+                  dummy2 = rep(1, nrow(tbl)),
+                  dummy3 = sample(c(0,1), nrow(tbl), TRUE),
+                  dummy4 = sample(c(0,1), nrow(tbl), TRUE),
+                  dummy5 = sample(c(0,1), nrow(tbl), TRUE),
+                  dummy6 = sample(c(0,1), nrow(tbl), TRUE)
+                  )
+    withActions <- filter(tbl, action != 'NO_ACTION')
+    withoutActions <- filter(tbl, action == 'NO_ACTION')
+    print(paste("With actions: ", nrow(withActions)))
+    print(paste("Without actions: ", nrow(withoutActions)))
+    
+    trainData <- rbind(sample_n(withActions,size = nrow(withoutActions),
+                                replace = TRUE),
+                                sample_n(withoutActions, 
+                                             nrow(withoutActions)))
+    trained <- randomForest(dummy6~., trainData)
+    par(mfrow = c(1,2), xpd = NA)
+    plot(trained)
+    text(trained)
+    trained
+}
+
+train_and_print(table)
 
